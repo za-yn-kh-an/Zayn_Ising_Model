9bdc0562e97ff66d558251949dd195728e96d993
za-yn-kh-an <125475446+za-yn-kh-an@users.noreply.github.com>
2024-11-05 12:59:32 +0000

    Add files via upload

--- Ising Model mk 1.py
+++ Ising Model mk 1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as random
+from scipy.constants import Boltzmann
 
 
 def initial_state(L):
@@ -19,17 +20,23 @@ def initial_state(L):
         A numpy array where each point represents a different spin.
     L : INT
         Defines the size of the lattice sides.
-
+    """
+    
     """
     lattice_points = np.zeros((L,L))
     up_or_down = [-1,1]
 
+    
     for i in range(L):
         for j in range(L):
             lattice_points[i,j] = random.choice(up_or_down)
+    """
+    
+    lattice_points = np.ones((L,L))
+    
     return lattice_points, L
 
-def Hamiltonian(lattice_points, L):
+def Hamiltonian(lattice_points, L, T):
     """
     Calculates the Hamiltonian of each microstate of the lattice.
     It also sets out periodic boundary conditions for the lattice edges.
@@ -40,6 +47,8 @@ def Hamiltonian(lattice_points, L):
         An array that contains the spins at each point of the lattice.
     L : INT
         Defines the size of the lattice sides.
+    T : FLOAT 64
+        Defines the temperature
 
     Returns
     -------
@@ -47,15 +56,18 @@ def Hamiltonian(lattice_points, L):
         The Hamiltonian of a given microstate of the lattice.
     w : FLOAT.64
         DESCRIPTION.
-
     """
+    #k = Boltzmann
+    k = 1
     H = 0; J = 1
+    
+    
     for i in range(L):
         for j in range(L):
             reference_point = lattice_points[i,j]
             above = lattice_points[i,j-1]
             left = lattice_points[i-1,j]
-            
+
             if i == L-1:
                 right = lattice_points[0,j]
             else:
@@ -65,29 +77,99 @@ def Hamiltonian(lattice_points, L):
                 below = lattice_points[i,0]
             else: 
                 below = lattice_points[i, j+1]
-            
-            H += J * reference_point * (above+below+right+left)
-    w = np.exp(-H/8)
+                
+            H += J * reference_point * (above+below+right+left) #J is ang. mom. (high J means less magnetisation)
+            w = np.exp(-H/(k*T))
+                    
     return H, w
 
-    
 
-def metropolis(L,reps):
+
+def metropolis(L,reps, T):
+    """
+    This function calculates the inital parameters of the lattice and then
+    randomly changes one spin at a time. If the change is accepted then the
+    magnetisation is calculated and applied to the results
+
+    Parameters
+    ----------
+    L : INT
+        DESCRIPTION.
+    reps : INT
+        DESCRIPTION.
+    T : INT
+        DESCRIPTION.
+
+    Returns
+    -------
+    sum_A : FLOAT 64
+        The sum of the magnetisation densities.
+    z : INT
+        DESCRIPTION.
+    N : INT
+        Number of iterations the model has gone through.
+
+    """
     sum_A = 0
     z = 0
     N = 0
+    H_list = []
+    sum_w = 0
     
     lattice_points, length = initial_state(L)
-    H,w = Hamiltonian(lattice_points, length)
+    H,w = Hamiltonian(lattice_points, length, T)
     
-    z +=1 ; N +=1
-    A = w/z; sum_A += A
+    #print(H)
+    #print(w)
+    
+    H_list.append(H)
+    sum_w += w
+    
+    A = sum(sum(lattice_points))/(L**2)
+    sum_A += A; z +=1 ; N +=1
     
     for k in range(reps):
         i = random.randint(0, L) - 1; j = random.randint(0, L) - 1
         lattice_points[i,j] = -1 * lattice_points[i,j]
-        H,w = Hamiltonian(lattice_points, L)
-        p = w/z
-        if p >= 0.5:
-            sum_A += p ; z += 1 ; N +=1
+        H,w = Hamiltonian(lattice_points, L, T)
+        
+        #print(w/sum_w)
+        
+        if w/sum_w >= 0.2:
+            sum_A += A ; z += 1 ; N +=1
+            H_list.append(H)
+            sum_w += w
+        
     return sum_A, z, N
+
+
+
+T = np.arange(0.2,3.2,0.2)
+T_len = len(T)
+"""
+A = np.zeros(15)
+reps = 1000
+for i in range(15):
+    for j in range(reps):
+        A[i] += (metropolis(4, 1000, T[i])[0])
+    A[i] = A[i]/reps
+#myInt = 100
+#new_A = [x / myInt for x in A]
+"""
+
+#%%
+
+C = np.zeros(T_len)
+reps = 1000
+for j in range(reps):
+    B = np.zeros(T_len)
+    for i in range(T_len):
+        B[i] += (metropolis(4, 1000, T[i])[0])
+    C = C + B
+A = np.divide(C,reps)
+
+
+
+plt.plot(T,A)
+plt.xlabel("Temperature")
+plt.ylabel("Magnetisation Density")
